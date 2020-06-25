990ed522641bdafe0892f009074975e4d008f3c2
yimgunho <https://github.com/yimgunho/TestServer>
2020-06-25 13:04:19 +0900

    2. class에 담기

---
+++ DataClass.py
@@ -0,0 +1,52 @@
+class DataClass:
+    def __init__(self):
+        self.sum_de = None
+        self.distrct_div_nm = None
+        self.emgncy_center_telno = None
+        self.refine_lotno_addr = None
+        self.refine_roadnm_addr = None
+        self.refine_wgs84_logt = None
+        self.refine_wgs84_lat = None
+
+    def out_sum_de(self):
+        return self.sum_de
+
+    def out_distrct_div_nm(self):
+        return self.distrct_div_nm
+
+    def out_emgncy_center_telno (self):
+        return self.emgncy_center_telno
+
+    def out_refine_lotno_addr(self):
+        return self.refine_lotno_addr
+
+    def out_refine_roadnm_addr(self):
+        return self.refine_roadnm_addr
+
+    def out_refine_wgs84_logt(self):
+        return float(self.refine_wgs84_logt)
+
+    def out_refine_wgs84_lat(self):
+        return float(self.refine_wgs84_lat)
+
+    def in_sum_de(self, data):
+        self.sum_de = data
+
+    def in_distrct_div_nm(self, data):
+        self.distrct_div_nm = data
+
+    def in_emgncy_center_telno(self, data):
+        self.emgncy_center_telno = data
+
+    def in_refine_lotno_addr(self, data):
+        self.refine_lotno_addr = data
+
+    def in_refine_roadnm_addr(self, data):
+        self.refine_roadnm_addr = data
+
+    def in_refine_wgs84_logt(self, data):
+        self.refine_wgs84_logt = data
+
+    def in_refine_wgs84_lat(self, data):
+        self.refine_wgs84_lat = data
+
--- main_project.py
+++ main_project.py
@@ -1,15 +1,72 @@
+import re
 import urllib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
+from DataClass import DataClass
 
-u = urllib.request.urlopen("https://openapi.gg.go.kr/EmgMedInfo?KEY=685efddff2354c78a02c17e9c9453169&Type=xml&pSize=1000")
+u = urllib.request.urlopen(
+    "https://openapi.gg.go.kr/EmgMedInfo?KEY=685efddff2354c78a02c17e9c9453169&Type=xml&pSize=1000")
 
 soup = BeautifulSoup(u, 'html.parser')
 
-print(soup)
+base = []
+base = [DataClass() for i in range(0, 1000)]
 
-# SUM_DEs = soup.findAll('sum_de')
-# for SUM_DE in SUM_DEs:
-#     SUM_DE = re.sub('<.*?>', '|', SUM_DE.text)
-#     print(SUM_DE)
+Sum_des = soup.findAll('sum_de')
+Distrct_div_nms = soup.findAll('distrct_div_nm')
+Emgncy_center_telnos = soup.findAll('emgncy_center_telno')
+Refine_lotno_addrs = soup.findAll('refine_lotno_addr')
+Refine_roadnm_addrs = soup.findAll('refine_roadnm_addr')
+Refine_wgs84_logts = soup.findAll('refine_wgs84_logt')
+Refine_wgs84_lats = soup.findAll('refine_wgs84_lat')
+
+cnt = 0
+for sum_de in Sum_des:
+    sum_de = re.sub('<.*?>', '|', sum_de.text)
+    base[cnt].in_sum_de(sum_de)
+    cnt = cnt + 1
+cnt = 0
+for distrct_div_nm in Distrct_div_nms:
+    distrct_div_nm = re.sub('<.*?>', '|', distrct_div_nm.text)
+    base[cnt].in_distrct_div_nm(distrct_div_nm)
+    cnt = cnt + 1
+cnt = 0
+for emgncy_center_telno in Emgncy_center_telnos:
+    emgncy_center_telno = re.sub('<.*?>', '|', emgncy_center_telno.text)
+    base[cnt].in_emgncy_center_telno(emgncy_center_telno)
+    cnt = cnt + 1
+cnt = 0
+for refine_lotno_addr in Refine_lotno_addrs:
+    refine_lotno_addr = re.sub('<.*?>', '|', refine_lotno_addr.text)
+    base[cnt].in_refine_lotno_addr(refine_lotno_addr)
+    cnt = cnt + 1
+cnt = 0
+for refine_roadnm_addr in Refine_roadnm_addrs:
+    refine_roadnm_addr = re.sub('<.*?>', '|', refine_roadnm_addr.text)
+    base[cnt].in_refine_roadnm_addr(refine_roadnm_addr)
+    cnt = cnt + 1
+cnt = 0
+for refine_wgs84_logt in Refine_wgs84_logts:
+    refine_wgs84_logt = re.sub('<.*?>', '|', refine_wgs84_logt.text)
+    base[cnt].in_refine_wgs84_logt(refine_wgs84_logt)
+    cnt = cnt + 1
+cnt = 0
+for refine_wgs84_lat in Refine_wgs84_lats:
+    refine_wgs84_lat = re.sub('<.*?>', '|', refine_wgs84_lat.text)
+    base[cnt].in_refine_wgs84_lat(refine_wgs84_lat)
+    cnt = cnt + 1
+
+for x in range(1000):
+    print(base[x].out_sum_de())
+for x in range(1000):
+    print(base[x].out_distrct_div_nm())
+for x in range(1000):
+    print(base[x].out_emgncy_center_telno())
+for x in range(1000):
+    print(base[x].out_refine_lotno_addr())
+for x in range(1000):
+    print(base[x].out_refine_roadnm_addr())
+for x in range(1000):
+    print(base[x].out_refine_wgs84_logt())
+for x in range(1000):
+    print(base[x].out_refine_wgs84_lat())
